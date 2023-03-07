5eed98d5e28918d2953e834fea098f58ee81558e
Lucas Rossi <99259327+LucasRossi3@users.noreply.github.com>
2023-03-07 10:43:56 -0300

    Add files via upload

--- Projetos/prjCirculo/src/Aplic.java
+++ Projetos/prjCirculo/src/Aplic.java
@@ -6,17 +6,17 @@ public class Aplic {
     public static void main(String[] args) {
         double raio;
         
-        circulo objRet = new circulo();
+        circulo objCirc = new circulo();
         Scanner entrada = new Scanner(System.in);
         
         System.out.print("Insira o valor do raio: ");
         raio = entrada.nextDouble();
-        objRet.setRaio(raio);
+        objCirc.setRaio(raio);
         
-        System.out.println("\nO raio é igual a: " + objRet.getRaio());
-        System.out.println("A área do círculo é igual a: " + objRet.calcArea());
-        System.out.println("O perímetro do círculo é igual a: " + objRet.calcPerimetro());
-        System.out.println("O diametro do cícrulo é igual a: " + objRet.calcDiametro());
+        System.out.println("\nO raio é igual a: " + objCirc.getRaio());
+        System.out.println("A área do círculo é igual a: " + objCirc.calcArea());
+        System.out.println("O perímetro do círculo é igual a: " + objCirc.calcPerimetro());
+        System.out.println("O diametro do cícrulo é igual a: " + objCirc.calcDiametro());
         
     }
     
