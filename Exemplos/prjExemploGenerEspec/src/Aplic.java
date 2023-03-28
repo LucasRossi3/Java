0b93e556c0301cd7e9b2283cd2f2a1e18e6031ca
Lucas Rossi <99259327+LucasRossi3@users.noreply.github.com>
2023-03-28 09:34:52 -0300

    Add files via upload

---
+++ Exemplos/prjExemploGenerEspec/src/Aplic.java
@@ -0,0 +1,25 @@
+
+import fatec.poo.model.Aluno;
+import fatec.poo.model.Professor;
+import java.text.DecimalFormat;
+
+public class Aplic {
+
+    public static void main(String[] args) {
+        
+        DecimalFormat df = new DecimalFormat ("#,##0.00");
+        
+        Aluno objAluno = new Aluno (1010, "Carlos Silveira", "15/03/1978");
+        Professor objProfessor = new Professor (2020, "Carlos Alberto", "09/07/1976");
+                
+        objAluno.setMensalidade(1500);
+        System.out.println("Registro Escolar: " + objAluno.getRegEscolar());
+        System.out.println("Nome: " + objAluno.getNome());
+        System.out.println("Data Nascimento: " + objAluno.getDataNascimento());
+        System.out.println("Mensalidade: " + objAluno.getMensalidade());
+        
+        
+        
+    }
+    
+}
